24d7549537a119d8cdcd2610bd2df269fd67bf87
MC <mch@e-komplet.dk>
2023-04-26 20:09:41 +0200

    made appbar type conditional based on device

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import DataComponent from './components/DataComponent.vue';
+import MobileAppbar from './components/MobileAppbar.vue';
+import DesktopAppbar from './components/DesktopAppbar.vue';
+import { isMobile } from './helpers/EnviorementHelper'
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const isMobileDevice = ref<boolean>(false)
+onMounted(() => { isMobileDevice.value = isMobile() }) 
 </script>
 
 <template>
   <v-app>
     <DataComponent />
     <v-main>
+      <MobileAppbar v-if="isMobileDevice" />
+      <DesktopAppbar v-else />
       <router-view />
     </v-main>
   </v-app>
