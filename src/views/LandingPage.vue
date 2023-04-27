56bed288afcf231962fb6d52575e9b9980e4fa84
MC <mch@e-komplet.dk>
2023-04-27 21:15:53 +0200

    Added invitation card to desktop view

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -3,17 +3,17 @@ import { isMobile } from '@/helpers/EnviorementHelper'
 import routes from '@/models/componentModels/Routes'
 import { useRouter } from 'vue-router'
 
-const description = `I anledningen af min 30 års fødselsdag holder jeg et lille arrangement her i Nordjylland, helt præcist afholdes det på Løkken stand og på Havregaarden.`
-const description2 = `Fordi der er flere som skal rejse langt, vil der være mulighed for overnatning, med den ene krølle at man skal sørge for at medbringe eget sengetøj.`
-const description3 = `Arrangementet starter officielt lørdag d. 24/6 kl 11:00 på Løkken strand. Dog er alle velkomne på Havregaarden allerede d. 23/6, fra kl 16:00.`
-const description4 = `Man skal selv sørge for transport til og fra stranden, der går busser, men ellers kan man indbyrdes aftale trnasport.`
-const description5 = `På stranden vil der være mulighed for at surfe (med indstruktør på), der er dog tilmelding til surf senest d. 16/6.`
-const description6 = `Ellers vil der være lidt forskellige spil for dem der er til det. Man kan også fint bare ligge og dase og snakke med alle de andre som er med. Undervejs er der kolde forfriskninger, snacks mv. og jeg sørger for frokost i form af nogle sandwiches.`
-const description7 = `Vi skyder festen igang Kl 17:00 på Havregaarden med lidt bobler. Planen er herefter at vi griller (vejret har dog lidt at sige her), hvis nogen har allergier, må de meget gerne melde det ind i god tid, så menuen kan opdateres derefter. Menuen kan ses under menu punktet.`
-const description8 = `Der vil ikke være noget serveringspersonale. Derfor er det høflig selvbetjening, og så gerne vi hjælper hinanden med at ryde af, inden festen rigtig starter.`
-const description9 = `Når maden er spist og vi har fået et stykke kage, bliver der skruet lidt op for musikken og med fri adgang i baren er jeg sikker på vi nok skal få en fremragende fest!`
-const description10 = `Glæder mig til at se jer alle i weekenden d. 23-25 Juni.`
-const description11 = `Kærlige hilsener fra Marc`
+const description = `I anledningen af min 30 års fødselsdag holder jeg et lille arrangement her i Nordjylland, helt præcist afholdes det på Løkken strand og på Havregaarden. 
+Fordi der er flere som skal rejse langt, vil der være mulighed for overnatning, med den ene krølle at man skal sørge for at medbringe eget sengetøj og håndklæder. 
+Arrangementet starter officielt lørdag d. 24/6 kl 11:00, på Løkken strand. Dog er alle velkomne på Havregaarden allerede d. 23/6, fra kl 16:00. 
+Man skal selv sørge for transport til og fra stranden, der går busser, men ellers kan man indbyrdes aftale transport. 
+På stranden vil der være mulighed for at surfe (med instruktør på), der er dog tilmelding til surf senest d. 16/6. 
+Ellers vil der være lidt forskellige spil for dem der er til det. Man kan også fint bare ligge og dase og snakke med alle de andre som er med. Undervejs er der kolde forfriskninger, snacks mv. og jeg sørger for frokost i form af nogle sandwiches. 
+Vi skyder festen igang Kl 17:00 på Havregaarden med lidt bobler. Planen er herefter at vi griller (vejret har dog lidt at sige her), hvis nogen har allergier, må de meget gerne melde det ind i god tid, så menuen kan opdateres derefter. Menuen kan ses under menu punktet. 
+Der vil ikke være noget serveringspersonale. Derfor er det høflig selvbetjening, og efter maden hjælpes vi ad med at ryde af og tage opvask. 
+Når maden er spist og vi har fået et stykke kage, bliver der skruet lidt op for musikken og med fri adgang i baren er jeg sikker på vi nok skal få en fremragende fest! 
+Glæder mig til at se jer alle i weekenden d. 23-25 Juni. 
+Kærlige hilsener fra Marc`
 
 const router = useRouter()
 const heroButtonRoutes = routes.getHeroButtonsRouteDetails()
@@ -24,13 +24,23 @@ const isMobileDevice = isMobile()
 </script>
 
 <template>
-  <v-container class="overlap-banner vertical-center btn-container"
-               v-if="isMobileDevice">
+  <v-container v-if="isMobileDevice"
+               class="overlap-banner vertical-center btn-container">
     <v-btn v-for="routeDetail in heroButtonRoutes"
            size="large"
            color="var(--vuetify-secondary)"
            @click="router.push(routeDetail.Route)">{{ routeDetail.Title }}</v-btn>
   </v-container>
+  <v-row v-else
+         class="overlap-banner horizontal-center">
+    <v-col cols="6"
+           offset="5">
+      <v-card title="Invitation"
+              subtitle="Marc's 30 års fødselsdag d. 23-25 Juni."
+              :text="description" />
+    </v-col>
+  </v-row>
+
   <video autoplay
          loop
          muted
@@ -38,19 +48,6 @@ const isMobileDevice = isMobile()
     <source src="../assets/video/4000-1_04925.mp4"
             type="video/mp4">
   </video>
-  <!-- <v-sheet>
-    <div>{{ description }}</div>
-    <div>{{ description2 }}</div>
-    <div>{{ description3 }}</div>
-    <div>{{ description4 }}</div>
-    <div>{{ description5 }}</div>
-    <div>{{ description6 }}</div>
-    <div>{{ description7 }}</div>
-    <div>{{ description8 }}</div>
-    <div>{{ description9 }}</div>
-    <div>{{ description10 }}</div>
-    <div>{{ description11 }}</div>
-  </v-sheet> -->
 </template>
 
 <style scoped>
