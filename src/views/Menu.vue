a8f876ec135635c0f87f23b0d4c1bf382287901a
MC <mch@e-komplet.dk>
2023-05-05 19:57:06 +0200

    Menu view placeholder

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 </script>
 
-<template></template>
+<template>
+    <v-container>
+        <div class="text-h3 text-center">Kommer på et senere tidspunkt</div>
+    </v-container>
+</template>
 
 <style scoped></style>
